9c5f838af3547c44b2ee0cf9035eaa945e666b91
Bharat Bhagat <bharat.bhagat@crisil.com>
2017-01-30 15:54:17 +0530

    adding logging

--- src/main/java/com/bharat/sampleQuickStart/App.java
+++ src/main/java/com/bharat/sampleQuickStart/App.java
@@ -1,5 +1,8 @@
 package com.bharat.sampleQuickStart;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Scanner;
 
 /**
@@ -8,10 +11,12 @@ import java.util.Scanner;
  */
 public class App 
 {
+	private static final Logger LOG = LoggerFactory.getLogger(App.class);
     public static void main( String[] args )
     {
+		LOG.info("app start");
     	readFromSystemIn();
-        System.out.println( "Hello World!" );
+        LOG.info("end of app");
     }
     
 	public static void readFromSystemIn() {
